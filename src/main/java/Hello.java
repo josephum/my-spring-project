4bbb7e1ea7b31845b1ee3f2c0f8103a056be80cb
Jose <josephum.garcia@gmail.com>
2022-02-12 10:51:04 -0500

    some updates 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hola!!!");
+
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
     }
 }
